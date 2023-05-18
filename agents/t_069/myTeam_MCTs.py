8fe107c8f2a6057c952a60d23e33deb052d074ab
Yu Hin Li <yuhinl@student.unimelb.edu.au>
2023-05-18 18:01:53 +1000

    Update MCTs node

--- agents/t_069/myTeam_MCTs.py
+++ agents/t_069/myTeam_MCTs.py
@@ -2,16 +2,65 @@ from template import Agent
 import time,random,heapq
 from copy import deepcopy
 from Azul.azul_model import AzulGameRule as GameRule
-
+from typing import Union, List
 from collections import deque
 
 THINKTIME   = 0.9
 NUM_PLAYERS = 2
+GAMMA = 0.9
+ALPHA = 0.6
+EPSILON = 0.5
+
+class MctNode(object):
+    def __init__(self,_id,state,parent,current_action):
+        self.q_value = 0
+        self.parent = parent
+        self.visit_times = 0
+        self.children: List[MctNode] = []
+        self.actions = self.GetActions(state,_id)
+        self.current_move = current_action
+        self.g_state: state
+        self.player_id = _id
+
+    # Generates actions from this state.
+    def GetActions(self, state, _id):
+        return self.game_rule.getLegalActions(state, _id)
+    
+    def isExpanded(self):
+        for child in self.children:
+            if child.visit_times == 0:
+                return False
+        return True
+    
+    def get_unexpanded_child(self):
+        for child in self.children:
+            if child.visit_times == 0:
+                return child
+        return None
+
+    def get_best_value_child(self):
+        best_child = self.children[0]
+        for child in self.children:
+            if child.q_value > best_child.q_value:
+                best_child = child
+        return best_child
+    
+    # Mean value backward-propergation 
+    def update(self):
+        self.visit_times += 1
+        s1, s2 = 0, 0
+        for child in self.children:
+            s1 += child.visit_times * child.q_value
+            s2 += child.visit_times
+        self.q_value = s1 / s2
+
+    def is_round_end(self):
+        return not self.g_state.TilesRemaining()
 
 class myAgent(Agent):
     def __init__(self,_id):
-            super().__init__(_id)
-            self.game_rule = GameRule(NUM_PLAYERS)
+        super().__init__(_id)
+        self.game_rule = GameRule(NUM_PLAYERS)
 
     # Generates actions from this state.
     def GetActions(self, state, _id):
@@ -43,5 +92,28 @@ class myAgent(Agent):
         for i in range(len(agent_state.floor)):
             penalty += agent_state.floor[i] * agent_state.FLOOR_SCORES[i]
         return penalty
+    
+
+    # MCTs
+    def SelectAction(self, actions, game_state):
+        best_action = self.bestRandomAction(actions)
+        
+        start_time = time.time()
+        initial_state = deepcopy(game_state)
+
+
+        return best_action
+    
+    def selectState(self, state):
+
+
+    # def isExpanded(self, state):
+
+    # def Expand(self, state):
+
+    # def Simulation():
+
+    # def BackPropagation():
+
     
 
