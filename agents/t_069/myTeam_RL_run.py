fa4b59adc58960bbec95b19966cb437bd5ffd28f
Kevenztl <kevenztl@gmail.com>
2023-05-19 17:51:59 +1000

    add reinforcement learning run part

--- agents/t_069/myTeam_RL_run.py
+++ agents/t_069/myTeam_RL_run.py
@@ -1,29 +1,62 @@
 from template import Agent
-import time,random,heapq, json
-from copy import deepcopy
 from Azul.azul_model import AzulGameRule as GameRule
+import random, time, json
+from copy import deepcopy
 
-from collections import deque
-
-THINKTIME   = 0.9
+THINKTIME = 0.9
 NUM_PLAYERS = 2
-ALPHA = 0.1
-GAMMA = 0.9
-EPSILON = 0.5
+
 
 class myAgent(Agent):
-    def __init__(self,_id):
+    def __init__(self, _id):
         super().__init__(_id)
         self.game_rule = GameRule(NUM_PLAYERS)
-        self.weight = [0,0,0,0,0,0]
-        with open("agents/t_069/RL_weight/weight_init.json",'r',encoding='utf-8')as w:
-            self.weight = json.load(w)['weight']
+        self.weight = [0, 0, 0, 0, 0, 0]
+        with open("agents/t_069/RL_weight/weight_init.json", "r", encoding='utf-8') as fw:
+            self.weight = json.load(fw)['weight']
         print(self.weight)
 
-    # Generates actions from this state.
-    def GetActions(self, state, _id):
-        return self.game_rule.getLegalActions(state, _id)
-    
-    # Carry out a given action on this state and return True if goal is reached received.
-    def DoAction(self, state, action, _id):
-        state = self.game_rule.generateSuccessor(state, action, _id)
+    def DoAction(self, state, action):
+        state = self.game_rule.generateSuccessor(state, action, self.id)
+
+
+    def CalFeatures(self, state, action):
+        features = []
+        next_state = deepcopy(state)
+        self.DoAction(next_state, action)
+        # F1 Floor line
+        floor_tiles = len(next_state.agents[self.id].floor_tiles)
+        features.append(floor_tiles / 7)
+        # F2-6 complete line 1-5
+        for i in range(5):
+            if next_state.agents[self.id].lines_number[i] == i+1:
+                features.append(1)
+            else:
+                features.append(0)
+        return features
+
+    def CalQValue(self, state, action):
+        features = self.CalFeatures(state, action)
+        if len(features) != len(self.weight):
+            print("F ansd W length not matched")
+            return -99999
+        else:
+            ans = 0
+            for i in range(len(features)):
+                ans += features[i] * self.weight[i]
+            return ans
+
+    def SelectAction(self, actions, game_state):
+        start_time = time.time()
+        best_action = random.choice(actions)
+        best_Q_value = -999999
+        if len(actions) > 1:
+            for action in actions:
+                if time.time() - start_time > THINKTIME:
+                    print("timeout")
+                    break
+                Q_value = self.CalQValue(game_state, action)
+                if Q_value > best_Q_value:
+                    best_Q_value = Q_value
+                    best_action = action
+        return best_action
--- agents/t_069/myTeam_RL_train_2.py
+++
@@ -1,2 +0,0 @@
-# from template import Agent
-# from Azul.azul_model import  import Au
