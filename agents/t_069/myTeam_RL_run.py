fb7a5b9f8f4bc398e5514b457c4d5dc07235a56b
Yu Hin Li <yuhinl@student.unimelb.edu.au>
2023-05-19 16:07:37 +1000

    Working on RL algorithm, initialise weight for RL

--- agents/t_069/myTeam_RL_run.py
+++ agents/t_069/myTeam_RL_run.py
@@ -16,22 +16,14 @@ class myAgent(Agent):
         super().__init__(_id)
         self.game_rule = GameRule(NUM_PLAYERS)
         self.weight = [0,0,0,0,0,0]
+        with open("agents/t_069/RL_weight/weight_init.json",'r',encoding='utf-8')as w:
+            self.weight = json.load(w)['weight']
+        print(self.weight)
 
     # Generates actions from this state.
     def GetActions(self, state, _id):
         return self.game_rule.getLegalActions(state, _id)
     
-    
     # Carry out a given action on this state and return True if goal is reached received.
     def DoAction(self, state, action, _id):
         state = self.game_rule.generateSuccessor(state, action, _id)
-
-    def GetScore(self, state, next_state, _id):
-        next_state = deepcopy(state)
-
-        self.DoAction(next_state, 'ENDROUND', _id)
-        oppoent_id = 1 - self.id
-        return (self.game_rule.calScore(next_state, _id)-self.game_rule.calScore(state,_id)) \
-                -(self.game_rule.calScore(next_state,oppoent_id)) - (self.game_rule.calScore(state,oppoent_id))
-    
-    
